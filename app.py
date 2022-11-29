ed004ac7e2ce47eac8fd1487e6e9602091cf13af
Jingye Xu <jingye.xu@outlook.com>
2022-11-29 18:02:56 -0500

    add self-signed certificates and check

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, url_for, redirect
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import pandas as pd
+from os.path import exists
+import sys
 
 
 app = Flask(__name__)
@@ -33,6 +35,14 @@ def delete(id):
 
 
 if __name__ == "__main__":
+
+    # check if self-signed certificates exist
+    if not exists("cert.pem") or not exists("key.pem"):
+        print("Does not find self-signed certificates, run command below in your terminal:")
+        print("openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 365")
+        sys.exit(1)
+
+    # check if database exists
     if "food" not in dbnames:
         db = client.food
         org_data = pd.read_csv("database.csv")
@@ -41,4 +51,4 @@ if __name__ == "__main__":
     
     db = client.food
     food_data = db.data
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000, ssl_context=('cert.pem', 'key.pem'))
